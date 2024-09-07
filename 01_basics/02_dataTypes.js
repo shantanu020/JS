0f9b2c9d11f9490e9228f06630309624f3184176
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-07 09:16:06 +0000

    dataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all Js code as newer version
+// alert("hello") we are using NodeJs, not browser
+//codde readability should be high
+let name="Shantanu"
+let age=19
+let isLoggedIn=true
+let state;
+//number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+//object
+console.log(typeof null)
+//null is a object
+console.log(typeof undefined)
