c1b0f5f03fb69c13ee72b97a5fffd8b46643a2b6
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-07 15:19:46 +0000

    nums and maths done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //used for precision consider only values after decimal
+
+
+const num=123.8869
+// console.log(num.toPrecision(3)); //consider all value before and after decimal point
+
+const hundreds =10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++ Maths ++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(3.48)) //roundOff
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(4,3,5,6,7))
+// console.log(Math.max(7,5,3,7,9,10))
+
+//to generate Random value
+// console.log(Math.random());
+
+//to generate a random value but it should be greater than 0
+// console.log(Math.floor(Math.random()*10)+1);
+
+//to generate random value between a range max - min
+const min=10;
+const max=20;
+// console.log(Math.floor((Math.random()*(max-min+1)))+min)
