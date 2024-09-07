fe05c45d385e0921cd80cc7efd14004dfd9de119
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-07 10:49:38 +0000

    comparisons done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2!=1)
+// console.log(2==1)
+
+// console.log("2">1) //true
+// console.log("02">1) //true
+// console.log("1">2) //false
+
+// console.log(null>0); //false
+// console.log (null==0); //false
+// console.log(null>=0); //true
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+//false in all case
+//so avoid this type of conversions and comparisons;
+
+
+
+//strict equal ===
+
+// console.log(2==="2"); //false
+
+
