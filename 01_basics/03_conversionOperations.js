ce67384857be16413f1983d3056080a80b468881
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-07 10:19:43 +0000

    conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+let score = "shan"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//  null => 0
+
+let isLoggedIn = "shan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+/*********************************operations*****************************/
+let value=9;
+let negValue=-value;
+// console.log(negValue)
+
+// console.log(2+9);
+// console.log(2**9);
+// console.log(9-2);
+// console.log(2*9);
+// console.log(9/2);
+// console.log(9%2);
+
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+2+"3")
+// console.log("1"+2+3)
+
+//if string comes first everything will be treated as string 
+//if number comes first everything will be treated as number
+
+// console.log(+true) => 1
+// console.log(+"") => 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
