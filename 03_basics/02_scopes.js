0221c455e2136516f45ae633e49b983d7b6edba0
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-08 14:22:31 +0000

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,4 +15,42 @@ for(let i=0;i<10;i++){
 }
 //the global scope of browser console is different as compaired to the code environment
 
+//nested scopes
+function one(){
+    const username="shan"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website)
+    two()
+}
+one()
 
+if (true) {
+    const username = "shan"
+    if (username === "shan") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+//++++++++++++++++++++++++ interesting ++++++++++++++++++++++++
+//hoisting
+//function
+console.log(addone(1))
+function addone(num){
+    return num+1
+}
+
+
+//expression
+// console.log(addTwo(0))
+const addTwo =function(num){
+    return num+2
+}
+console.log(addTwo(7))
