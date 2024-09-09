719e5f3ca1bb571804f8a8e041555fb6f3d8bc15
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-09 14:58:18 +0000

    for loop done

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,37 @@
+// for
+
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
+
+//nested for loop
+
+// for(let i=1;i<=10;i++){
+//     console.log("Outer Loop: ",i);
+//     for(let j=1;j<=10;j++){
+//         // console.log(`inner loop ${j}`)
+//         console.log(i+'*'+j+' = '+i*j);
+//     }
+// }
+
+// let heroes=["ironman","batman","flash","spiderman","superman"];
+// for(let i=0;i<heroes.length;i++){
+//     console.log(heroes[i]);
+// }
+//there is no array index out of bound instead here we have undefined
+
+
+//Keywords break and continue
+
+// for(let i=1;i<=20;i++){
+//     if(i==2){
+//         console.log("2 detected");
+//         continue; //skips current iteration
+//     }
+//     if(i==5){
+//         console.log("detected 5");
+//         break; //to break control flow
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
