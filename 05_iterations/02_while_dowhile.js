5722357272db131798fceb5f3dd12583d31fee61
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-09 15:04:39 +0000

    while and do while loop done

---
+++ 05_iterations/02_while_dowhile.js
@@ -0,0 +1,29 @@
+// while (condition) {
+    
+// }
+
+// let i=0;
+// while(i<=10){
+//     console.log(`value of i is ${i}`);
+//     i++;
+// }
+
+// let heroes=["shaktiman","naagraj","doga"];
+// let i=0;
+// while(i<heroes.length){
+//     console.log(`hero: ${heroes[i]}`);
+//     i++;
+// }
+//++++++++++++++++++++
+// do {
+    
+// } while (condition);
+
+// let score=11;
+// do{
+//     console.log(score)
+//     score++;
+// }
+// while(score<=10);
+
+//at least one iteration is executed even if condition is wrong
