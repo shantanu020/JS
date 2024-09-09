afc03f79813727208b2f436c7446847aa494af3a
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-09 13:59:42 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+//switch case statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//value can be any data type (string,numbers)
+
+// const month =3
+
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     default:
+//         console.log("default case match")
+//         break;
+// }
+
+//if we don't apply break then all the cases will be executed after getting case match except default
+//default case will only be executed in case of no match
