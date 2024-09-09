a6c4006f3dc8717f94e645c38cfddb6865e79565
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-09 13:54:18 +0000

    if else basics done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// if
+// const isUserLoggedIn=true
+// if(isUserLoggedIn){
+
+// }
+
+//comparison operators
+// <,>,<=,>=,!=,==,===
+//=== checks types as well
+
+
+
+//if else
+
+
+// if(isUserLoggedIn){
+
+// }else{
+
+// }
+
+
+//shorthand notation
+// const bal=1000
+// if(bal>500)console.log("test"); 
+// if(bal>500)console.log("test"),console.log("testagain");  // correct but this is bad practice 
+
+// if-else if
+
+// if(bal<500){
+//     console.log("less than 500")
+// }else if(bal<700){
+//     console.log("less than 700")
+// }else{
+//     console.log("greater than 700")
+// }
+
+//multiple conditions
+
+// const userLoggedIn=true
+// const debbitCard=true
+// const loggedInFromGoogle=false
+// const loggedInFromEmail=true
+
+// if(userLoggedIn&&debbitCard){
+//     console.log("allow")
+// }
+
+// if(loggedInFromEmail||loggedInFromGoogle){
+//     console.log("allow")
+// }
