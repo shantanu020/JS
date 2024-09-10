aa5816dc386c5249999714cd59ec0271dde4f933
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-10 13:48:08 +0000

    for in loop done

---
+++ 05_iterations/04_iterating_object.js
@@ -0,0 +1,38 @@
+//for-in loop
+//++++++++++++++++++++++
+
+//object
+
+// const obj={
+//     js:"JavaScript",
+//     cpp:"C++",
+//     rb:"ruby",
+//     swift:"swift by apple"
+// }
+// for (const key in obj) {
+//     console.log(`${key} shortcut for ${obj[key]}`);
+// }
+//it return the keys not values directly
+//+++++++++++++++++++++++++++
+
+//Array
+
+// const lang=["c","c++","java","python"]
+// for (const key in lang) {
+//    console.log(`${key} : ${lang[key]}`) 
+// }
+//for in loop returns the index of array
+//++++++++++++++++++++++++++
+
+//map
+
+// const map=new Map()
+// map.set('IN','India')
+// map.set('USA','United States of America')
+// map.set('IN','India')
+
+// for(const key in map){
+//     console.log(key)
+// }
+
+//Map is not iterable so we can't use for in loop for it
