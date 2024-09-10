5b671b88d6db5e09113b378dd457fd45f471b707
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-09-10 15:39:50 +0000

    map and chaining done

---
+++ 05_iterations/08_map_chaining.js
@@ -0,0 +1,11 @@
+const nums=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums=nums.map((i)=>i+10) //it doesn't modify original array 
+// console.log(nums)
+// console.log(newNums);
+
+//++++++++++++++++++++++++++++
+
+//Chaining
+// const newNums=nums.map((i)=>i*10)
+// .filter((i)=>i%2===0)
+// console.log(newNums)
